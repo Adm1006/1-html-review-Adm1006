f2c1b2cc2ae396c00e29bd42b55023b7d42a4d37
Adm1006 <89426090+Adm1006@users.noreply.github.com>
2021-09-21 17:47:53 -0400

    commit

---
+++ app/public/js/randomperson.js
@@ -0,0 +1,28 @@
+const RandPerson = {
+    data() {
+      return {
+        "person": {},
+      }
+    },
+
+computed: {
+    prettyBirthday() {
+        return dayjs(this.person.dob.date).format('D MMM YYYY');
+    }
+},
+
+methods: {
+    fetchUserData(){
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((parsedJson) => {
+            console.log(parsedJson);
+            this.person = parsedJson.results[0]
+        })
+        .catch( err => {
+            console.error(err)
+        })
+    }
+}
+  
+Vue.createApp(RandPerson).mount('#InfoApp');
